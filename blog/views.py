544d8f568cb56e62b23a716094a0e3f1bf6f3ca9
Temuulen <temka9196@gmail.com>
2020-09-14 16:15:21 +0800

    Chanaged the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def test2(request):
+    return HttpResponse("Testing views, urls")
